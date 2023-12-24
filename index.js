9f89074fe006aed722642dad36c6fa970c1b7334
ArunrajMK <arunrajshanker6@gmail.com>
2023-12-24 10:44:50 +0530

    updated

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT||8080
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use("/signup", signupRoutes);
 app.use("/signin", signinRoutes);
 app.use("/", homeRoutes);
@@ -30,5 +28,4 @@ try{
 catch(error){
     console.log("error connecting to db",error)
 }
-
 })
