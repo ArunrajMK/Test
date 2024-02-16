39639e6c8e72cb0f3debf74b87f1541b2c1cad97
Anusha-surendrankp <anusha@medpick.in>
2024-02-16 23:31:48 +0530

    updated

--- routes/signin.routes.js
+++ routes/signin.routes.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (user) {
-        // If the user exists, compare the provided password with the hashed password stored in the database
+      
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (passwordMatch) {
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
             const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
             res.json({ message: 'Login Success', token, name: user.name, firm_name: user.firm_name, mobile: user.mobile, email: user.email });
         } else {
-            // Password does not match
+     
             res.status(401).json({ message: 'Login failed: Incorrect password' });
         }
     } else {
